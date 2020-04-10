38220f1bf3fe07e0108f1a3119a8c2d2ffbdb305
anthony <aahewins@protonmail.com>
2020-04-10 13:25:10 -0400

    workflow and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+![build](https://github.com/AnthonyHewins/sec-company-valuation/workflows/build/badge.svg)
+
+SEC company valuation
+=====================
+
+Uses _[discounted cash flow
+(DCF)]__(https://www.investopedia.com/terms/d/dcf.asp) to evaluate a company's
+stock price.
+
+Makes use of the SEC's records of annual company filings to make a conservative
+estimate of the stock price.
--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -10,10 +10,10 @@ import (
 )
 
 func main() {
-	secYear       := flag.Int(     "y",  time.Now().Year(), "Using DERA, fetch financial statements directly from the SEC's website")
-	secQuarter    := flag.Int(     "q",                  1, "Using DERA, fetch financial statements directly from the SEC's website")
-	thirdPartyUrl := flag.String("url",                 "", "Fetch the financial statements elsewhere (still must match schema)")
-	localZip      := flag.String("zip",                 "", "Use a local ZIP to load the data (most favorable option)")
+	secYear       := flag.Int(     "y", time.Now().Year(), "Using DERA, fetch financial statements directly from the SEC's website")
+	secQuarter    := flag.Int(     "q",                 1, "Using DERA, fetch financial statements directly from the SEC's website")
+	thirdPartyUrl := flag.String("url",                "", "Fetch the financial statements elsewhere (still must match schema)")
+	localZip      := flag.String("zip",                "", "Use a local ZIP to load the data (most favorable option)")
 
 	flag.Parse()
 
